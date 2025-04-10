ff33c51a542aa7bb72aa8a319132ba404183a798
hbsnow <takahashi.yuki@classmethod.jp>
2025-04-10 09:57:50 +0900

    refactor(page): simplify dataset generation by removing label parameter from generateDataset function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { LineGraph } from "@/components/graph/line";
 import { BarGraph } from "@/components/graph/bar";
 import { MixedGraph } from "@/components/graph/mixed";
 
-const generateDataset = (label: string) =>
+const generateDataset = () =>
   [...Array(9)].map((_, i) => ({
     name: String.fromCharCode(65 + i), // 65は'A'のUnicode値
     value: Math.floor(Math.random() * 100),
@@ -10,19 +10,25 @@ const generateDataset = (label: string) =>
 
 export default function Home() {
   const data = [
-    { label: "Email", data: generateDataset("Email") },
-    { label: "Direct", data: generateDataset("Direct") },
+    {
+      label: "Email",
+      data: generateDataset(),
+    },
+    {
+      label: "Direct",
+      data: generateDataset(),
+    },
   ];
   const mixedData = [
     {
       type: "bar" as const,
       label: "Union Ads",
-      data: generateDataset("Union Ads"),
+      data: generateDataset(),
     },
     {
       type: "line" as const,
       label: "Video Ads",
-      data: generateDataset("Video Ads"),
+      data: generateDataset(),
     },
   ];
 
